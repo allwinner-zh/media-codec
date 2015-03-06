50e15b46c2e9e4d01a7a290b0d5720e740419d9e
kevin.z.m <kevin@allwinnertech.com>
2015-03-06 18:22:33 +0800

    clean the open source
    1. add license anouncement to the source code file;
    2. shape the library files;
    
    Signed-off-by: kevin.z.m <kevin@allwinnertech.com>

--- sunxi-cedarx/LIBRARY/config.h
+++ sunxi-cedarx/LIBRARY/config.h
@@ -1,4 +1,20 @@
-
+/*
+* Cedarx framework.
+* Copyright (c) 2008-2015 Allwinner Technology Co. Ltd.
+* Copyright (c) 2014 BZ Chen <bzchen@allwinnertech.com>
+*
+* This file is part of Cedarx.
+*
+* Cedarx is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This program is distributed "as is" WITHOUT ANY WARRANTY of any
+* kind, whether express or implied; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*/
 #ifndef CONFIG_H
 #define CONFIG_H
 
